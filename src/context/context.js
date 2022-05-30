bf6a46a438c36b567873ff5730ae9e87b2441dd4
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-30 07:34:45 -0700

    Added : useContext

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,15 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+// export create context and cont
+// context provider
+
+const GithubContext = React.createClass();
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={'d'}>{children}</GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
