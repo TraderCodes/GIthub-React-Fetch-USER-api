178224a32839f528ee6e98f86eb3fa2ebe58fd86
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-30 10:39:43 -0700

    Added : user seciton

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,46 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    bio,
+    blog,
+    location,
+    twitter_username,
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'joker'}</p>
+        </div>
+        <a href={html_url}>Follow</a>
+      </header>
+      <p className="bio"> {bio} </p>
+      {/* Icon section */}
+      <div className="links">
+        <p>
+          <MdBusiness />
+          {company || 'None'}
+        </p>
+        <p>
+          <MdLocationOn />
+          {location || 'yo'}
+        </p>
+
+        <a href={`https://${blog}`}>
+          <MdLink /> {blog || ' None'}
+        </a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
